7120b971d6ce7c19d6e608f0e735d3cc5750c2b4
MikioOsaki <32075804+MikioOsaki@users.noreply.github.com>
2018-05-17 15:30:34 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var locationPicker = require("location-picker");
+var fileUpload = require('express-fileupload');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html', function (err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
+});
+
+app.listen(port, function () {
+    console.log('Gulp is running my app on PORT: ' + port);
+});
